7f3dfae3a494a091a503bdb01da5101a6ff07aa3
CISG-Olympus <86250320+CISG-Olympus@users.noreply.github.com>
2021-10-12 09:45:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ module "terraform-intersight-iks" {
   }
   # Cluster information
   cluster = {
-    name                = "iks-zeus"
+    name                = "iks-zeus01"
     action              = "Deploy"
     wait_for_completion = false
     worker_nodes        = 2
